e0343bfd5b7471d776742f2e79c857977358e1e9
dcallaway <dustin_callaway@intuit.com>
2021-05-14 23:15:23 +0200

    Exclude DS store file from zip file

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,1 +1,1 @@
-cd .. && zip -r tab-boss.zip . --exclude=*.git* --exclude=*.zip* --exclude=*scripts* --exclude=*README.md*
+cd .. && zip -r tab-boss.zip . --exclude=*.git* --exclude=*.zip* --exclude=*scripts* --exclude=*README.md* --exclude=*.DS_Store*
