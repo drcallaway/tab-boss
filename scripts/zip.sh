edf1b035b1ef7e918854a19211fe8dcbd43439a2
dcallaway <dustin_callaway@intuit.com>
2021-05-21 15:50:02 +0200

    Move zip.sh script to root

--- scripts/zip.sh
+++
@@ -1,1 +0,0 @@
-cd .. && zip -r tab-boss.zip . --exclude=*.git* --exclude=*.zip* --exclude=*scripts* --exclude=*README.md* --exclude=*.DS_Store*
---
+++ zip.sh
@@ -0,0 +1,1 @@
+zip -r tab-boss.zip . --exclude=*.git* --exclude=*.zip* --exclude=*scripts* --exclude=*README.md* --exclude=*.DS_Store*
